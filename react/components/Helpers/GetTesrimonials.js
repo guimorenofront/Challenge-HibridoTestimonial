25919dc8f5c89924df2e89b652a2527a528be426
Guilherme <guilherme.dev0210@outlook.com>
2022-03-08 15:37:28 -0300

    Remove keys

--- react/components/Helpers/GetTesrimonials.js
+++ react/components/Helpers/GetTesrimonials.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 export const GetTestimonials = () => {
   const [data, setData] = useState([]);
   //pr001 fazendo a Consulta ao masterData e Retornando o conteudo
-  function Request(keys) {
+  function Request() {
     fetch(`/api/dataentities/TC/search?_fields=testimonial,name,clientcity,data,imgurl`, {
       method: "get",
       headers: {
